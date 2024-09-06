a79282ac8fd1e760038fbd71efc95a6b785e796e
TiannaLopes <tiannaalinalopes@gmail.com>
2024-09-06 07:54:51 -0400

    base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
+  output: "export",
+  basePath: "/dsa-guide",
+  assetPrefix: "/dsa-guide/"
 };
 
 export default nextConfig;
